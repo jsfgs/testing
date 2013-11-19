ffba5d8b83988b9e20b0171beae080b83a09b6a8
Jan Sau <sunyss@inbox.lv>
2013-11-19 18:55:26 +0200

    Simple Console added

---
+++ simple_console/simple_console/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace simple_console
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
